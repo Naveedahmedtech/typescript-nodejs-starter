41b242ac0aef4238c608040f01a84781543120ab
Naveedahmedtech <technaveedahmed@gmail.com>
2024-07-13 22:58:29 +0500

    2 error files added to gitigoner | remove unnecessary code

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,6 @@
 import { router } from "@/config/express.config";
 import { registerUser } from "@/controller/auth";
 
-router.get("/register", registerUser);
+router.post("/register", registerUser);
 
 export default router;
