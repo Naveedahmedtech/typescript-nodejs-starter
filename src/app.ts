cdc7d6ce0e58d8ec697832e3c8ba339d49f7629c
Naveedahmedtech <technaveedahmed@gmail.com>
2024-07-15 10:10:44 +0500

    small changes

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import routes from '@/routes'
 export const createApp = () => {
     app.use(requestLogger);
     app.use(express.json());
-    app.use("/", routes);
+    app.use("/api", routes);
     app.use(notFoundHandler);
     app.use(errorHandler);
     return app;
--- src/utils/responseHandler.ts
+++ src/utils/responseHandler.ts
@@ -1,14 +1,18 @@
 import { Response } from "@/config/express.config";
+import { RESPONSE_CODES } from "@/constants";
+
 export const sendSuccessResponse = (
   res: Response,
   message: string,
   result: any = null,
-  statusCode: number = 200
+  statusCode: number = 200,
+  code: string = RESPONSE_CODES.SUCCESS
 ) => {
   res.status(statusCode).json({
-    code: statusCode,
-    message,
     success: true,
+    statusCode,
+    code,
+    message,
     result,
   });
 };
@@ -19,15 +23,53 @@ export const sendErrorResponse = (
   statusCode = 500,
   stack: string | null = null
 ) => {
+  let code: string;
+
+  switch (statusCode) {
+    case 400:
+      code = RESPONSE_CODES.BAD_REQUEST;
+      break;
+    case 401:
+      code = RESPONSE_CODES.UNAUTHORIZED;
+      break;
+    case 403:
+      code = RESPONSE_CODES.FORBIDDEN;
+      break;
+    case 404:
+      code = RESPONSE_CODES.NOT_FOUND;
+      break;
+    case 409:
+      code = RESPONSE_CODES.CONFLICT;
+      break;
+    case 422:
+      code = RESPONSE_CODES.UNPROCESSABLE_ENTITY;
+      break;
+    case 201:
+      code = RESPONSE_CODES.CREATED;
+      break;
+    case 202:
+      code = RESPONSE_CODES.ACCEPTED;
+      break;
+    case 204:
+      code = RESPONSE_CODES.NO_CONTENT;
+      break;
+    case 500:
+    default:
+      code = RESPONSE_CODES.INTERNAL_SERVER_ERROR;
+      break;
+  }
+
   const response: {
-    code: number;
+    code: string;
+    statusCode: number;
     message: string;
     success: boolean;
     stack?: string | null;
   } = {
-    code: statusCode,
-    message,
     success: false,
+    statusCode,
+    code,
+    message,
   };
 
   if (process.env.NODE_ENV !== "prod" && stack) {
